78aa70040abc6bfd6963fb55db8c265a5e5317d5
Samuel Mooney <lappletop@Samuels-MacBook-Pro.local>
2021-09-13 12:23:23 -0400

    button implemented

--- main.js
+++ main.js
@@ -1,36 +1,25 @@
 /*
-Learning Objectives
-You should know how to use html and css selectors
-Performance Objectives
-Demonstrate basic knowledge: html, and css selectors
-Deliverables
-A GitHub repository containing an
-index.html
-styles.css
-main.js
-normalize.css
 I'm a Web Developer Mode
-Create an HTML file
-create a new project folder in your code directory named awesomely-ugly-website and add an HTML file named index.html
-Practice HTML
-practice the basics of HTML, create a simple web page with some HTML content; the uglier and more HTML you can muster, the better
-use a minimum of 10 different html elements
-Add Normalize.css to your web page
-Add some custom styles
-create a stylesheet named styles.css and add css selectors and rules
+use a minimum of 10 _different_ html elements
 use a minimum of 10 different css style attributes
 use a minimum of 4 different css selectors
-Add at least one image to your awesomely ugly site
 Use the float property to wrap text around an image
 Add a button to your webpage that swaps out an image when you click it
-Deploy your site to GitHub
+
+
 Hey, Mikey, I Think He Likes It Mode
 Sadly, the <blink> tag has been removed from html, re-implement it with CSS animations!
 Scrolling marques really help grab the attention of would-be-customers, so add one to your site
 Add a footer to your site with an "under construction" animated gif
+
 Caffeinated Mode
 Create a web page layout that has two columns of content that are side-by-side
 Add a table of made up statistic calculations to your site (Like "# of Happy Vegetable Eaters by State")
-
-
 */
+
+const $duck = document.querySelector('#duck');
+const $duckButton = document.querySelector('button');
+$duckButton.addEventListener('click', function (event) {
+    $duck.setAttribute('src',"https://upload.wikimedia.org/wikipedia/commons/7/79/Operation_Upshot-Knothole_-_Badger_001.jpg");
+    console.log('Button pushed!');
+})
